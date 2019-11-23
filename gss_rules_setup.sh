e306a1be50955fd925970951341c99b78effc29c
vishawanathjadhav <vjadhav@redhat.com>
2019-11-24 03:20:40 +0530

    Fixed gss-rules repo setup issue

--- gss_rules_setup.sh
+++ gss_rules_setup.sh
@@ -11,11 +11,13 @@ PACKAGE_DEPS="git unzip python-pip python36"
 CRNT_USR=$(whoami)
 if [ "${CRNT_USR}" != "root" ];then
 	echo "ERROR: Not a root user"
-	return 2
+	exit 1
 fi
 
-
 function create_user(){
+	echo "===================="
+	echo "Create insights user"
+	echo "===================="
 	# Create User!
 	useradd -d ${INSIGHTS_HOME} ${INSIGHTS_USR} -s /bin/bash
 	echo "Create password for ${INSIGHTS_USR}"
@@ -41,7 +43,9 @@ function source_insights_user(){
 
 
 function register_system() {
+	echo "===================="
 	echo "Register the system!"
+	echo "===================="
 	# Register the user
 	sudo subscription-manager register --force
 	# Enable EMPLOYEE SKU
@@ -56,16 +60,22 @@ function register_system() {
 
 
 function installed_deps() {
-	echo "Install the deps!"
+	echo "===================="
+	echo "Install Dependencies"
+	echo "===================="
 	sudo yum install -y ${PACKAGE_DEPS}
 	sudo pip3 install virtualenv
+	sudo pip install -e .[develop]
 }
 
 
 function create_virt_env() {
+	echo "==================="
+	echo "Create virtualenv"
+	echo "==================="
 	mkdir -pv ${INSIGHTS_WKSP}
 	mkdir -pv ${INSIGHTS_ENV}
-	chown insights:insights -R ${INSIGHTS_HOME}
+	sudo chown insights:insights -R ${INSIGHTS_HOME}
 	cd ${INSIGHTS_ENV}
 	virtualenv .
 
@@ -73,27 +83,53 @@ function create_virt_env() {
 
 
 function setup_insights_core() {
+	echo "==================="
 	echo "Clone insights-core"
+	echo "==================="
 	cd ${INSIGHTS_WKSP}
 	source ./ienv/bin/activate
 	git clone https://github.com/RedHatInsights/insights-core.git 
-	chown insights:insights -R insights-core
+	sudo chown insights:insights -R .
 	cd ${INSIGHTS_CORE}
 	echo $(pwd)
 	pip3 install -e .[develop]
+	pip install -e .[develop]
 }
 
 function setup_gss_rules() {
+	echo "==============="
 	echo "Clone gss-rules"
+	echo "==============="
 	cd ${INSIGHTS_WKSP}
-	git clone https://gitlab.cee.redhat.com/support-insights/gss-rules.git
-	chown insights:insights -R gss-rules
+	git clone git@gitlab.cee.redhat.com:support-insights/gss-rules.git
+	if [ $? -ne 0 ]; then
+		echo "===================================="
+		echo "WARNING: Please check ssh permission"
+		echo "Generate ssh key, press enter"
+		echo "===================================="
+		ssh-keygen
+		echo "======================================================="
+		echo 'Add: following ssh-key to "User Settings" >> "SSH Keys"'
+		echo "======================================================="
+		cat ~/.ssh/id_rsa.pub
+		read -p "Continue[Y/y]?" -n 1 -r
+		rm -rf gss-rules
+		git clone git@gitlab.cee.redhat.com:support-insights/gss-rules.git
+	fi
+	sudo chown insights:insights -R .
+	sudo chown insights:insights -R ${INSIGHTS_ENV}
 	cd ${GSS_RULES}
 	echo $(pwd)
 	pip3 install -e .[develop]
+	pip install -e .[develop]
 }
 
 function test_insights_core() {
+	echo "========================"
+	echo "Test insights-core setup"
+	echo "========================"
+	sudo chown insights:insights -R .
+	sudo chown insights:insights -R ${INSIGHTS_ENV}
 	py.test .
 	if [ -z $? ];then
 		echo "SUCESS: Insights core framework setup done"
@@ -103,19 +139,35 @@ function test_insights_core() {
 }
 
 function test_gss_rules() {
+	echo "===================="
+	echo "Test gss-rules setup"
+	echo "===================="
+	sudo chown insights:insights -R .
+	sudo chown insights:insights -R ${INSIGHTS_ENV}
+	cd ${INSIGHTS_CORE}
+	pip3 install -e .[develop]
+	pip install -e .[develop]
+	cd ${GSS_RULES}
 	py.test .
-	if [ -z $? ];then
+	resutlt="$?"
+	if [ "$result" -eq 0 ];then
+		echo "==================================="
+		echo "             SUCESS                "
+		echo "==================================="
 		echo "SUCESS: gss-rules setup complete"
 		echo "Login the system as insights"
 		echo "cd /home/insights/workspace/"
 		echo "source ./ienv/bin/activate"
+		echo "==================================="
 	else
 		echo "ERROR: gss-rules setup failed"
+		echo "============================="
 	fi
 }
 
 if [ -d "${INSIGHTS_HOME}" ];then
-	echo "ERROR: Insights user alread present"
+	echo "INFO: insights user already present"
+	echo "==================================="
 else
 	create_user
 fi
